a8c2175839df1ae4a75a4ea55c143b6590e472a3
shafiqulislamshawon <shafiqshawonofficial@gmail.com>
2025-02-22 12:00:10 +0600

    first commit

--- WebCmsService/settings.py
+++ WebCmsService/settings.py
@@ -80,14 +80,23 @@ WSGI_APPLICATION = 'WebCmsService.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mydatabase',
+        'USER': 'dbmasteruser',
+        'PASSWORD': 'sZi)L(^nk:7p`x[i&S!`*~bk83Lig;DH',
+        'HOST': 'ls-eece619cae21379b158abf94d901c94c9d82b253.cvgfhs4irem7.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
