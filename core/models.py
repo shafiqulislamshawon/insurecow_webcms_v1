c424a480510a5a6c5f1c1350e5476ce93b6dc927
shafiqulislamshawon <shafiqshawonofficial@gmail.com>
2025-03-02 13:00:24 +0600

    dynamic card added

--- core/models.py
+++ core/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
@@ -87,3 +89,24 @@ class Testimonial(models.Model):
 
     def __str__(self):
         return self.name
+
+class BaseCategory(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class BaseCard(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='BaseCard/')
+    category = models.ForeignKey(BaseCategory, on_delete=models.CASCADE)
+    extra_data = models.JSONField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
